deaf571d90ba816cb73f4b10205f293b38422b6b
vsensu <vsensu@foxmail.com>
2021-04-06 12:28:26 +0800

    Game Objects and Scripts

---
+++ Assets/CatlikeCoding/2.2Basics/1GameObjectsAndScripts/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private Transform hoursPivot;
+    [SerializeField] private Transform minutesPivot;
+    [SerializeField] private Transform secondsPivot;
+
+    private const float hoursToDegree = -30f, minutesToDegree = -6f, secondsToDegree = -6f;
+    
+    void Update()
+    {
+        var time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f,  hoursToDegree * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegree * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegree * (float)time.TotalSeconds);
+    }
+}
