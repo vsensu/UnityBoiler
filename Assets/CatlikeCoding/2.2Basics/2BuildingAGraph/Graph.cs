46f32f102ba7d9e62bb6a75d6a70f27407684a10
vsensu <vsensu@foxmail.com>
2021-04-07 18:19:59 +0800

    Building a graph

---
+++ Assets/CatlikeCoding/2.2Basics/2BuildingAGraph/Graph.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] private Transform pointPrefab;
+    [SerializeField, Range(10, 100)] private int resolution = 10;
+
+    private Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        var position = Vector3.zero;
+        points = new Transform[resolution];
+        for (int i = 0; i < resolution; i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+        } 
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < resolution; i++)
+        {
+            var point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
